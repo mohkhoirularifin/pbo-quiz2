2be8476d33dc14b73e63fdf2e25b9c8aa5bcd86c
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-11-30 11:09:01 +0700

    Pembuatan Class Transaksi dan Attributnya

---
+++ src/com/khoirularifin/quiz2/Transaksi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.khoirularifin.quiz2;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author KINTUL
+ */
+public class Transaksi {
+    private double total=0;
+    private DefaultTableModel tabel = new DefaultTableModel();
+}
